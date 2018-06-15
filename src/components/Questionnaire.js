f8562f7b64d7e4ef5ac5be34095f185a888facb4
Andy Zhong <Andy.Zhong@libertymutual.com%>
2018-06-15 11:27:52 -0400

    added createUser function in actions.js and updated signin functionality in questionnaire.js

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -4,73 +4,99 @@ import { Link } from 'react-router-dom';
 import { skills } from "../constants/technologyConstants";
 import CheckBox from './CheckBox';
 import { createUser } from '../redux/actions/actions';
-//import SignIn from './SignIn';
+import SignIn from './SignIn';
 
 class Questionnaire extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      id: Math.floor((Math.random() * 1000) + 1),
-      skills: [],
-      interests: []
-    };
-  }
-
-  addSkills(value, skill) {
-    if (value) {
-        this.setState(prevState => ({
-            skills: [...prevState.skills, skill]
-        }))
+        this.state = {
+            id: Math.floor((Math.random() * 1000) + 1),
+            skills: [],
+            interests: []
+        };
     }
-}
 
-addInterests(value, interest) {
-    if (value) {
-        this.setState(prevState => ({
-            interests: [...prevState.interests, interest]
-        }))
+    addSkills(value, skill) {
+        if (value) {
+            this.setState(prevState => ({
+                skills: [...prevState.skills, skill]
+            }))
+        }
     }
-}
 
-  render() {
-      return (
-          <div>
-                      <div className='flex-container-column align-center'>
-                          <img className='logo' src={require('../assets/M-logo.svg')} alt="logo"/>
-                          <div>
-                              Tell us about yourself
-                          </div>
+    addInterests(value, interest) {
+        if (value) {
+            this.setState(prevState => ({
+                interests: [...prevState.interests, interest]
+            }))
+        }
+    }
 
-                          <fieldset>
-                              <legend>List the top 3 skills you want to teach</legend>
-                              <CheckBox interest={skills.CSS} onInputChange={(value, interest) => this.addSkills(value, interest)} />
-                              <CheckBox interest={skills.HTML} onInputChange={(value, interest) => this.addSkills(value, interest)} />
-                              <CheckBox interest={skills.JAVA} onInputChange={(value, interest) => this.addSkills(value, interest)} />
-                              <CheckBox interest={skills.JAVASCRIPT} onInputChange={(value, interest) => this.addSkills(value, interest)} />
-                              <CheckBox interest={skills.MONGO} onInputChange={(value, interest) => this.addSkills(value, interest)} />
-                              <CheckBox interest={skills.REACT} onInputChange={(value, interest) => this.addSkills(value, interest)} />
-                          </fieldset>
+    createProfile() {
+        console.log('creating profile');
+        console.log('skills', this.state.skills);
+        console.log('interests ', this.state.interests);
 
-                          <fieldset>
-                              <legend>List the top 3 skills you want to learn</legend>
-                              <CheckBox interest={skills.CSS} onInputChange={(value, interest) => this.addInterests(value, interest)} />
-                              <CheckBox interest={skills.HTML} onInputChange={(value, interest) => this.addInterests(value, interest)} />
-                              <CheckBox interest={skills.JAVA} onInputChange={(value, interest) => this.addInterests(value, interest)} />
-                              <CheckBox interest={skills.JAVASCRIPT} onInputChange={(value, interest) => this.addInterests(value, interest)} />
-                              <CheckBox interest={skills.MONGO} onInputChange={(value, interest) => this.addInterests(value, interest)} />
-                              <CheckBox interest={skills.REACT} onInputChange={(value, interest) => this.addInterests(value, interest)} />
-                          </fieldset>
+        const user = {
+            'id': this.state.id,
+            'email': this.props.user.email,
+            'name': this.props.user.name,
+            'skills': this.state.skills,
+            'interests': this.state.interests
+        };
 
-                          <Link to={`/profile/${this.state.id}`}>
-                              <img src={require('../assets/Button-Hover.svg')} alt="button-next"
-                                         onClick={() => this.createProfile()}
-                              />
-                          </Link>
-                      </div>
-          </div>
-      );
-  }
+        this.props.createUser(user);
+    }
+
+    render() {
+        return (
+            <div>
+                { Object.keys(this.props.user).length !== 0  ?
+                    (
+                        <div>
+                            {this.props.isAuth ? '' : <SignIn />}
+                        </div>
+                    ) :
+                    (
+                        <div className='flex-container-column align-center'>
+                            <img className='logo' src={require('../assets/M-logo.svg')} alt="logo"/>
+                            <div>
+                                Tell us about yourself
+                            </div>
+
+                            <fieldset>
+                                <legend>List the top 3 skills you want to teach</legend>
+                                <CheckBox interest={skills.CSS} onInputChange={(value, interest) => this.addSkills(value, interest)} />
+                                <CheckBox interest={skills.HTML} onInputChange={(value, interest) => this.addSkills(value, interest)} />
+                                <CheckBox interest={skills.JAVA} onInputChange={(value, interest) => this.addSkills(value, interest)} />
+                                <CheckBox interest={skills.JAVASCRIPT} onInputChange={(value, interest) => this.addSkills(value, interest)} />
+                                <CheckBox interest={skills.MONGO} onInputChange={(value, interest) => this.addSkills(value, interest)} />
+                                <CheckBox interest={skills.REACT} onInputChange={(value, interest) => this.addSkills(value, interest)} />
+                            </fieldset>
+
+                            <fieldset>
+                                <legend>List the top 3 skills you want to learn</legend>
+                                <CheckBox interest={skills.CSS} onInputChange={(value, interest) => this.addInterests(value, interest)} />
+                                <CheckBox interest={skills.HTML} onInputChange={(value, interest) => this.addInterests(value, interest)} />
+                                <CheckBox interest={skills.JAVA} onInputChange={(value, interest) => this.addInterests(value, interest)} />
+                                <CheckBox interest={skills.JAVASCRIPT} onInputChange={(value, interest) => this.addInterests(value, interest)} />
+                                <CheckBox interest={skills.MONGO} onInputChange={(value, interest) => this.addInterests(value, interest)} />
+                                <CheckBox interest={skills.REACT} onInputChange={(value, interest) => this.addInterests(value, interest)} />
+                            </fieldset>
+
+
+                            <Link to={`/profile/${this.state.id}`}>
+                                <img src={require('../assets/Button-Hover.svg')} alt="button-next"
+                                           onClick={() => this.createProfile()}
+                                />
+                            </Link>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
@@ -80,4 +106,6 @@ const mapStateToProps = state => {
     }
 };
 
-export default Questionnaire;
+export default connect(mapStateToProps, {
+    createUser
+})(Questionnaire);
