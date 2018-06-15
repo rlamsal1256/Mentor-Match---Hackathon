03cdcf84354af768328605c59888708f4c0a295b
Rupak Lamsal <rupak.lamsal@libertymutual.com%>
2018-06-15 07:02:20 -0400

    added build

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const path = require('path');
 
 const app = express();
 const router = express.Router();
@@ -29,6 +30,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(helmet());
 //app.use('/static', express.static(path.join(__dirname, 'static')))
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/api', router);
 
