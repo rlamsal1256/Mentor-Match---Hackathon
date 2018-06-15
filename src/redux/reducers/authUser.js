0f45c475e0562b88e200f92832dca4270a833f4c
Andy Zhong <Andy.Zhong@libertymutual.com%>
2018-06-15 11:54:27 -0400

    Fixed merge thing

--- src/redux/reducers/authUser.js
+++ src/redux/reducers/authUser.js
@@ -1,18 +1,3 @@
-<<<<<<< 2f4aedc5a102c7d8e9ef3453c1c0b41c0452c6de
-const initialState = {};
-
-export default (state = initialState, action) => {
-    switch (action.type) {
-        case '':
-            return {
-                ...state,
-                hodor: action.hodor
-            };
-        default:
-            return state;
-    }
-}
-=======
 const initialState = {
     user: {},
     isAuth: false,
@@ -32,4 +17,3 @@ export default (state=initialState, action) => {
             return state;
     }
 }
->>>>>>> Added in redux stuff for authUser and Users and hooked it up to Questionnaire component
