251a7b983fe5c72835a91bcc5161a45d5d29dab1
Andy Zhong <Andy.Zhong@libertymutual.com%>
2018-06-15 11:52:11 -0400

    Fixed authUser merge conflict

--- src/redux/reducers/authUser.js
+++ src/redux/reducers/authUser.js
@@ -1,3 +1,4 @@
+<<<<<<< 2f4aedc5a102c7d8e9ef3453c1c0b41c0452c6de
 const initialState = {};
 
 export default (state = initialState, action) => {
@@ -11,3 +12,24 @@ export default (state = initialState, action) => {
             return state;
     }
 }
+=======
+const initialState = {
+    user: {},
+    isAuth: false,
+    profile: {}
+};
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                isAuth: Object.keys(action.user).length > 0,
+                user: action.user
+            };
+
+        default:
+            return state;
+    }
+}
+>>>>>>> Added in redux stuff for authUser and Users and hooked it up to Questionnaire component
